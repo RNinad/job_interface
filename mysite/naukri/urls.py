1b29003502f671c053c1dead7a3a913b487b5dc8
Ninad Rajandekar <ninadsidh@gmail.com>
2017-06-13 16:42:26 +0530

    front end design

--- mysite/naukri/urls.py
+++ mysite/naukri/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name= 'index'),	
 	url(r'^create_jobs/$', views.createJobs, name= 'cjobs'),
+	url(r'^display_jobs/$', views.displayJobs, name= 'djobs'),
+	url(r'^applications/$', views.dispApplications, name= 'applications'),
+	url(r'^apply/$', views.apply, name= 'apply'),
+	url(r'^submit_application/$', views.submitApplication, name= 'submitapp'),
+	url(r'^status/$', views.dispStatus, name= 'status'),
 ]
--- mysite/naukri/views.py
+++ mysite/naukri/views.py
@@ -10,3 +10,17 @@ def index(request):
 def createJobs(request):
 	return render(request, 'createJobs.html', {})
 	
+def displayJobs(request):
+	return render(request, 'displayJobs.html', {})
+
+def dispApplications(request):
+	return render(request, 'displayApplications.html', {})
+
+def apply(request):
+	return render(request, 'apply.html', {})
+
+def submitApplication(request):
+	return render(request, 'submitApplication.html', {})
+
+def dispStatus(request):
+	return render(request, 'displayStatus.html', {})
